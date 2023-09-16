4f6b6511ee2fab729bfcb3912be5c553fff73622
khalid khan <khalidkhan70004@gmail.com>
2023-09-16 16:09:12 +0500

    Add project files.

---
+++ DatabaseFirstDotNetCore/Models/DatabaseFirstDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DatabaseFirstDotNetCore.Models
+{
+    public partial class DatabaseFirstDbContext : DbContext
+    {
+        public DatabaseFirstDbContext()
+        {
+        }
+
+        public DatabaseFirstDbContext(DbContextOptions<DatabaseFirstDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.Designation).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.StudentGender).HasMaxLength(50);
+
+                entity.Property(e => e.StudentName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
